5b10baf87872d491f64198337c4303b042f5bfee
KBeKind <kyle@kbekind.com>
2024-01-05 10:34:42 -0600

    commit

--- main.py
+++ main.py
@@ -53,17 +53,17 @@
 # assertRaises()
 # assertIn()
 
-# import unittest
-# def add(x, y):
-#     return x + y
+import unittest
+def add(x, y):
+    return x + y
 
-# class MyTest(unittest.TestCase):
-#     def test_add(self):
-#         self.assertEqual(add(2, 3), 5)
+class MyTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
 
 
-# if __name__ == '__main__':
-#     unittest.main(verbosity=2)
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
 
 #----------------------------
